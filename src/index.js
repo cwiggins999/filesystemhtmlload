1a59a389e8380472386a372860b89555c9d69695
Chuck Wiggins <cwiggins999@gmail.com>
2023-05-24 15:58:50 -0400

    Demo of using web on storage device with plugins

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import '@capacitor/core';
 import {  Device} from '@capacitor/device';
 import { Filesystem,Directory, Encoding} from '@capacitor/filesystem';
+import { WebView } from '@capacitor/core';
 
 export async function getDeviceInfo() {
     let info = await Device.getInfo();
     return info;
   };
+  
 
   window.FSPlugin = Filesystem;
   window.DirectoryEnum = Directory;
+  window.WebView = WebView;
